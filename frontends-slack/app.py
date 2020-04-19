53cb3c2fb0bec2c4c1aa4b68ea9b47baeb8f53c6
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-04-19 13:38:54 +0100

    Add and run black

--- frontends-slack/app.py
+++ frontends-slack/app.py
@@ -9,10 +9,12 @@ from settings import (
 
 slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, endpoint="/slack/events")
 
+
 @slack_events_adapter.on("message")
 def handle_message(event_data):
     print(event_data)
     message = event_data["event"]
     print(message)
+
 
 slack_events_adapter.start(host=SERVER_HOST, port=SERVER_PORT, debug=DEBUG)
--- frontends-slack/settings.py
+++ frontends-slack/settings.py
@@ -1,6 +1,6 @@
 import os
 
-DEBUG = bool(os.environ.get('DEBUG', False))
-SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
-SERVER_HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
-SERVER_PORT = os.environ.get('SERVER_PORT', 80)
+DEBUG = bool(os.environ.get("DEBUG", False))
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+SERVER_HOST = os.environ.get("SERVER_HOST", "0.0.0.0")
+SERVER_PORT = os.environ.get("SERVER_PORT", 80)
