08ca2114f1cb33a0c533ebcb07a1579a57784ad3
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-04-19 14:16:34 +0100

    Add handler for member joining a channel

--- frontends-slack/app.py
+++ frontends-slack/app.py
@@ -17,4 +17,11 @@ def handle_message(event_data):
     print(message)
 
 
+@slack_events_adapter.on("member_joined_channel")
+def handle_member_joined_channel(event_data):
+    print(event_data)
+    member_joined = event_data["event"]
+    print(member_joined)
+
+
 slack_events_adapter.start(host=SERVER_HOST, port=SERVER_PORT, debug=DEBUG)
