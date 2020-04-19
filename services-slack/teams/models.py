69994b7d081e18080a597605bb3d12d89bfc487b
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-04-19 21:59:45 +0100

    Add teams app and model for slack service

---
+++ services-slack/teams/models.py
@@ -0,0 +1,10 @@
+import uuid
+
+from django.db import models
+
+
+class Team(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    team_id = models.CharField(max_length=255)
+    bot_user_id = models.CharField(max_length=255)
+    bot_access_token = models.CharField(max_length=255)
