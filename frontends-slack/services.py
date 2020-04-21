7df345e01f88cac0ef8fbd2ca26b001d7cb390da
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-04-21 21:54:22 +0100

    Post Slack events to service

--- frontends-slack/services.py
+++ frontends-slack/services.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 
 logger = logging.getLogger("events")
 
@@ -6,4 +7,12 @@ logger = logging.getLogger("events")
 def service_request(service_name, endpoint, payload):
     logger.debug(
         "service_request: %s %s %s", service_name, endpoint, payload,
+    )
+
+    schema = "http"
+    service_prefix = "services-"
+    url = f"{schema}://{service_prefix}{service_name}{endpoint}"
+
+    requests.post(
+        url, data=payload,
     )
