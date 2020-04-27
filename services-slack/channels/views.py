bc4fa82bc5bd5c0402b8ebc2788b43c2b10bb852
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-04-27 20:00:41 +0100

    Refactor creating members from a channel

--- services-slack/channels/views.py
+++ services-slack/channels/views.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 
 from slack import WebClient
@@ -13,6 +14,19 @@ from .models import Channel
 
 
 class MemberView(View):
+    def get_members_of_channel(self, channel):
+        team = channel.team
+        client = WebClient(token=team.bot_access_token)
+
+        members_responses = (
+            response["members"]
+            for response in client.conversations_members(channel=channel.slack_id)
+        )
+        members = itertools.chain(*members_responses)
+        members = (member for member in members if member != team.bot_user_id)
+
+        return members
+
     def post(self, request):
         event = json.loads(request.body)
         event_data = event["event"]
@@ -22,19 +36,15 @@ class MemberView(View):
         if team.bot_user_id == event_data["user"]:
             channel = Channel.objects.create(slack_id=event_data["channel"], team=team)
 
-            client = WebClient(token=team.bot_access_token)
-            for response in client.conversations_members(channel=channel.slack_id):
-                for member in response["members"]:
-                    if member == team.bot_user_id:
-                        continue
-
-                    try:
-                        member = Member.objects.get(slack_id=member, team=team,)
-                    except Member.DoesNotExist:
-                        person_response = service_request("people", "/person/", {})
-                        person_id = person_response["id"]
-                        Member.objects.create(
-                            person_id=person_id, slack_id=member, team=team,
-                        )
+            members = self.get_members_of_channel(channel)
+            for member in members:
+                try:
+                    member = Member.objects.get(slack_id=member, team=team)
+                except Member.DoesNotExist:
+                    person_response = service_request("people", "/person/", {})
+                    person_id = person_response["id"]
+                    Member.objects.create(
+                        person_id=person_id, slack_id=member, team=team,
+                    )
 
         return HttpResponse("OK")
